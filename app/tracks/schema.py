cd84e7111b5953c1405e55495c46cfbd82c3e02d
brigsalejo-streamlineverify <brigido.alejo@streamlineverify.com>
2020-03-15 14:23:11 +0800

    changed Exception to GraphQLError

--- app/tracks/schema.py
+++ app/tracks/schema.py
@@ -1,5 +1,6 @@
 import graphene
 from graphene_django import DjangoObjectType
+from graphql import GraphQLError
 from .models import Track, Like
 from users.schema import UserType
 
@@ -36,7 +37,7 @@ class CreateTrack(graphene.Mutation):
     def mutate(self, info, title, description, url):
         user = info.context.user
         if user.is_anonymous:
-            raise Exception('Log in to add a track.')
+            raise GraphQLError('Log in to add a track.')
 
         track = Track(title=title, description=description, url=url, posted_by=user)
         track.save()
@@ -54,11 +55,11 @@ class CreateLike(graphene.Mutation):
     def mutate(self, info, track_id):
         user = info.context.user
         if user.is_anonymous:
-            raise Exception('Login to like tracks.')
+            raise GraphQLError('Login to like tracks.')
 
         track = Track.objects.get(id=track_id)
         if not track:
-            raise Exception('Cannot find track with given track id')
+            raise GraphQLError('Cannot find track with given track id')
 
         Like.objects.create(
             user=user,
@@ -81,7 +82,7 @@ class UpdateTrack(graphene.Mutation):
         user = info.context.user
         track = Track.objects.get(id=track_id)
         if track.posted_by != user:
-            raise Exception('Not permitted to update this track.')
+            raise GraphQLError('Not permitted to update this track.')
 
         track.title = title
         track.description = description
@@ -101,7 +102,7 @@ class DeleteTrack(graphene.Mutation):
         user = info.context.user
         track = Track.objects.get(id=track_id)
         if track.posted_by != user:
-            raise Exception('Not permitted to delete this track.')
+            raise GraphQLError('Not permitted to delete this track.')
 
         track.delete()
 
