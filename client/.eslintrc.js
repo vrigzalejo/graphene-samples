8d7cbda89a7200b909a9838c7fa4bce37f28c710
brigsalejo-streamlineverify <brigido.alejo@streamlineverify.com>
2020-03-15 17:55:21 +0800

    added client frontend

---
+++ client/.eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    jest: true
+  },
+  extends: [
+    'plugin:react/recommended',
+    'standard',
+    'eslint:recommended'
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly'
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module'
+  },
+  plugins: [
+    'react'
+  ],
+  rules: {
+    "semi": ["error", "always"],
+    "quotes": ["error", "double"]
+  }
+}
