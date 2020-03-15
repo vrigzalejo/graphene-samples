3b82f1a7c69e615fc9c55c9b52ba66b74497f824
brigsalejo-streamlineverify <brigido.alejo@streamlineverify.com>
2020-03-15 13:30:25 +0800

    added CreateLike

--- app/tracks/schema.py
+++ app/tracks/schema.py
@@ -1,6 +1,7 @@
 import graphene
 from graphene_django import DjangoObjectType
-from .models import Track
+from .models import Track, Like
+from users.schema import UserType
 
 
 class TrackType(DjangoObjectType):
@@ -33,6 +34,32 @@ class CreateTrack(graphene.Mutation):
 
         return CreateTrack(track=track)
 
+
+class CreateLike(graphene.Mutation):
+    user = graphene.Field(UserType)
+    track = graphene.Field(TrackType)
+
+    class Arguments:
+        track_id = graphene.Int(required=True)
+
+    def mutate(self, info, track_id):
+        user = info.context.user
+        if user.is_anonymous:
+            raise Exception('Login to like tracks.')
+
+        track = Track.objects.get(id=track_id)
+        if not track:
+            raise Exception('Cannot find track with given track id')
+
+        Like.objects.create(
+            user=user,
+            track=track
+        )
+
+        return CreateLike(user=user, track=track)
+
+
+    
 
 class UpdateTrack(graphene.Mutation):
     track = graphene.Field(TrackType)
@@ -78,4 +105,5 @@ class Mutation(graphene.ObjectType):
     create_track = CreateTrack.Field()
     update_track = UpdateTrack.Field()
     delete_track = DeleteTrack.Field()
+    create_like = CreateLike.Field()
     
